479918a4589c67475ee3bf98f34b4fff3da96422
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2019-10-01 22:36:20 +0900

    Add curl NK225 large of latest

--- build.sbt
+++ build.sbt
@@ -1,4 +1,8 @@
 val `ref-data` = (project in file("ref-data"))
   .settings(
-    libraryDependencies += "org.wvlet.airframe" %% "airframe-log" % "19.9.9.2"
+    libraryDependencies ++=
+      Seq(
+        "org.wvlet.airframe" %% "airframe-log" % "19.9.9.2",
+        "org.jsoup" % "jsoup" % "1.12.1"
+      )
   )
--- ref-data/src/main/scala/yokohama/murataku/trade/refdata/Curl225Navi.scala
+++ ref-data/src/main/scala/yokohama/murataku/trade/refdata/Curl225Navi.scala
@@ -1,7 +1,26 @@
 package yokohama.murataku.trade.refdata
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import wvlet.log.LogSupport
 
+import scala.collection.JavaConverters
+
 object Curl225Navi extends App with LogSupport {
-  info("running")
+  val doc: Document = Jsoup.connect("http://225navi.com/data/").get()
+
+  import JavaConverters._
+
+  doc.select(".sc_table tr").asScala //
+    .drop(2) // drop headers
+    .map { row =>
+      val date = row.select("th").asScala.head.text()
+      val data = row.select("td").asScala
+      val open = data.head.text()
+      val high = data(1).text()
+      val low = data(2).text()
+      val close = data(3).text()
+
+      info(s"[$date][open=$open][high=$high][low=$low][close=$close]")
+    }
 }
