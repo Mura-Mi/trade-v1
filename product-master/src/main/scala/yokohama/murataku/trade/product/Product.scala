922fa12ef26db8a78db51f3498a23f4cd127981a
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2019-10-04 01:17:44 +0900

    wip: Index Future id

--- product-master/src/main/scala/yokohama/murataku/trade/product/Product.scala
+++ product-master/src/main/scala/yokohama/murataku/trade/product/Product.scala
@@ -1,14 +1,22 @@
 package yokohama.murataku.trade.product
 
+import java.time.LocalDate
+import java.util.UUID
+
+import yokohama.murataku.trade.holiday.YearMonth
+
 trait Product
 
-case class Index(name: String)
+case class Index(id: UUID, name: String)
 
-case class IndexFuture(productName: String, deliveryLimit: String)
+case class IndexFuture(indexId: UUID,
+                       productName: String,
+                       deliveryLimit: YearMonth,
+                       deliveryDate: LocalDate)
     extends Product
 
-case class IndexOption(productName: String,
-                       underlyingProductName: String,
+case class IndexOption(indexId: UUID,
+                       productName: String,
                        putOrCall: PutOrCall,
                        deliveryLimit: String,
                        strike: BigDecimal)
