3500e3337d8b4e05f5cea433545f5d0752d89e3f
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2019-10-29 08:44:53 +0900

    Optimize import

--- webapp/src/app/volatility/volatility.service.ts
+++ webapp/src/app/volatility/volatility.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {DailyVolatility} from "./DailyVolatility";
 import {Observable} from "rxjs";
 
