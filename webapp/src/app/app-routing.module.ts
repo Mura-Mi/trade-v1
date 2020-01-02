23d932cbeade12fd1c48e16a7f8932c5182a1290
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2020-01-02 12:21:04 +0900

    Add menu routing

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import {RouterModule, Routes} from '@angular/router';
 import {VolatilityComponent} from "./volatility/volatility.component";
 import {GreeksComponent} from "./greeks/greeks.component";
 import {GreeksComposeComponent} from "./greeks-compose/greeks-compose.component";
+import {MenuComponent} from "./menu/menu.component";
 
 
 const routes: Routes = [
+  {path: '', component: MenuComponent},
   {path: 'vol', component: VolatilityComponent},
   {path: 'greeks/:delivery/:strike/:poc/:valuationDate', component: GreeksComponent},
   {path: 'greeks-compose/:delivery/:date', component: GreeksComposeComponent},
